937f7467f62a2412d3fcd345f310d323d13a7b84
kabir1922 <kabir1922@gmail.com>
2025-10-17 22:46:58 +0600

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,1 +1,9 @@
-
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+  $name = htmlspecialchars($_POST['name']);
+  $email = htmlspecialchars($_POST['email']);
+  $message = htmlspecialchars($_POST['message']);
+  // mail() or store in DB goes here
+  echo "Thank you, $name! I’ll get back to you soon.";
+}
+?>
